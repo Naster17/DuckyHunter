31fb5bcab3c74913a08814d3234b3164cb4d02ad
Naster17 <nekjk17042005@gmail.com>
2023-03-01 23:15:04 +0100

    debug: hid and anroid api work

--- core/android_api.py
+++ core/android_api.py
@@ -1,13 +1,16 @@
+import os
+from config import *
+
 def LED_ON(led):
-    # os.popen(f"echo 254 > {led}")
+    os.popen(f"echo 254 > {led}")
     pass
 def LED_OFF(led):
-    # os.popen(f"echo 254 > {led}")
+    os.popen(f"echo 0 > {led}")
     pass
 def LEDF_ON(ledf):
-    # os.popen(f"echo 254 > {led}")
+    os.popen(f"echo 254 > {ledf}")
     pass
 def LEDF_OFF(ledf):
-    # os.popen(f"echo 254 > {led}")
+    os.popen(f"echo 0 > {ledf}")
     pass
 
